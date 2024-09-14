5e349df1eb4ea0a531e1a19a6c582ada0c24ef34
Alex Davidchuk <sasha.d1990@list.ru>
2024-09-15 01:24:21 +0400

    Added mock tests

---
+++ src/test/java/ru/ssau/tk/DoubleA/javalabs/functions/MockTabulatedFunctionTest.java
@@ -0,0 +1,25 @@
+package ru.ssau.tk.DoubleA.javalabs.functions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MockTabulatedFunctionTest {
+
+    final private static double epsilon = 0.00000001;
+
+    @Test
+    void test() {
+        MockTabulatedFunction mockTabulatedFunction = new MockTabulatedFunction(4.0, 6.0, 3.5, 2.4);
+        double x0 = mockTabulatedFunction.leftBound();
+        double x1 = mockTabulatedFunction.rightBound();
+        double y0 = mockTabulatedFunction.getY(0);
+        double y1 = mockTabulatedFunction.getY(1);
+        Assertions.assertTrue(Math.abs(2.675 - mockTabulatedFunction.interpolate(5.5, x0, x1, y0, y1)) <= epsilon);
+        Assertions.assertTrue(Math.abs(3.775 - mockTabulatedFunction.interpolate(3.5, x0, x1, y0, y1)) <= epsilon);
+        Assertions.assertTrue(Math.abs(0.75 - mockTabulatedFunction.interpolate(9.0, x0, x1, y0, y1)) <= epsilon);
+        Assertions.assertTrue(Math.abs(3.5 - mockTabulatedFunction.apply(4.0)) <= epsilon);
+        Assertions.assertTrue(Math.abs(4.6 - mockTabulatedFunction.apply(2.0)) <= epsilon);
+        Assertions.assertTrue(Math.abs(0.2 - mockTabulatedFunction.apply(10.0)) <= epsilon);
+        Assertions.assertTrue(Math.abs(2.95 - mockTabulatedFunction.apply(5.0)) <= epsilon);
+    }
+}

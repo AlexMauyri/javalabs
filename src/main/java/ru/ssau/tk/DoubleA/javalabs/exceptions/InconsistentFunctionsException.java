d9dae06a74a58041a2290665051bed8717aca825
Alex Davidchuk <sasha.d1990@list.ru>
2024-09-21 17:46:17 +0400

    Added binary operations for tabulated functions

---
+++ src/main/java/ru/ssau/tk/DoubleA/javalabs/exceptions/InconsistentFunctionsException.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.DoubleA.javalabs.exceptions;
+
+public class InconsistentFunctionsException extends RuntimeException {
+    public InconsistentFunctionsException() {
+    }
+
+    public InconsistentFunctionsException(String message) {
+        super(message);
+    }
+}

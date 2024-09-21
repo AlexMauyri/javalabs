7c8f7e9642f2c99a27e6d65ef4706de4c96f1e4d
Alex Davidchuk <sasha.d1990@list.ru>
2024-09-21 19:18:42 +0400

    Added new methods for factory

--- src/main/java/ru/ssau/tk/DoubleA/javalabs/functions/factory/TabulatedFunctionFactory.java
+++ src/main/java/ru/ssau/tk/DoubleA/javalabs/functions/factory/TabulatedFunctionFactory.java
@@ -1,7 +1,26 @@
 package ru.ssau.tk.DoubleA.javalabs.functions.factory;
 
+import ru.ssau.tk.DoubleA.javalabs.functions.StrictTabulatedFunction;
 import ru.ssau.tk.DoubleA.javalabs.functions.TabulatedFunction;
+import ru.ssau.tk.DoubleA.javalabs.functions.UnmodifiableTabulatedFunction;
 
 public interface TabulatedFunctionFactory {
     TabulatedFunction create(double[] xValues, double[] yValues);
+
+    default TabulatedFunction createUnmodifiable(double[] xValues, double[] yValues) {
+        return new UnmodifiableTabulatedFunction(create(xValues, yValues));
+    }
+
+    default TabulatedFunction createStrict(double[] xValues, double[] yValues) {
+        return new StrictTabulatedFunction(create(xValues, yValues));
+    }
+
+    default TabulatedFunction createStrictUnmodifiable(double[] xValues, double[] yValues) {
+        return new StrictTabulatedFunction(
+                new UnmodifiableTabulatedFunction(
+                        create(xValues, yValues)
+                )
+        );
+    }
+
 }

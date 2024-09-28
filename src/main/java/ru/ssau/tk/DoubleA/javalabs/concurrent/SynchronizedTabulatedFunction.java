51aeeabd1a99d04bc9b09a74ebff238c9b4fcb85
A9uamar1n <a9uamar1n.0522@gmail.com>
2024-09-28 10:47:17 +0400

    Added Synchronized Tabulated Function class

---
+++ src/main/java/ru/ssau/tk/DoubleA/javalabs/concurrent/SynchronizedTabulatedFunction.java
@@ -0,0 +1,111 @@
+package ru.ssau.tk.DoubleA.javalabs.concurrent;
+
+import ru.ssau.tk.DoubleA.javalabs.functions.Point;
+import ru.ssau.tk.DoubleA.javalabs.functions.TabulatedFunction;
+import ru.ssau.tk.DoubleA.javalabs.operations.TabulatedFunctionOperationService;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SynchronizedTabulatedFunction implements TabulatedFunction {
+    final TabulatedFunction tabulatedFunction;
+
+    public SynchronizedTabulatedFunction(TabulatedFunction tabulatedFunction) {
+        this.tabulatedFunction = tabulatedFunction;
+    }
+
+    @Override
+    public int getCount() {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.getCount();
+        }
+    }
+
+    @Override
+    public double getX(int index) {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.getX(index);
+        }
+    }
+
+    @Override
+    public double getY(int index) {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.getY(index);
+        }
+    }
+
+    @Override
+    public void setY(int index, double value) {
+        synchronized (tabulatedFunction) {
+            tabulatedFunction.setY(index, value);
+        }
+    }
+
+    @Override
+    public int indexOfX(double x) {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.indexOfX(x);
+        }
+    }
+
+    @Override
+    public int indexOfY(double y) {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.indexOfY(y);
+        }
+    }
+
+    @Override
+    public double leftBound() {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.leftBound();
+        }
+    }
+
+    @Override
+    public double rightBound() {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.rightBound();
+        }
+    }
+
+    @Override
+    public Iterator<Point> iterator() {
+        synchronized (tabulatedFunction) {
+            Point[] points = TabulatedFunctionOperationService.asPoints(tabulatedFunction);
+
+            return new Iterator<>() {
+                private Point currentPoint = points[0];
+                private int internalCount = 0;
+
+                @Override
+                public boolean hasNext() {
+                    return internalCount < points.length;
+                }
+
+                @Override
+                public Point next() {
+                    if (currentPoint == null) throw new NoSuchElementException();
+
+                    Point pointToReturn = currentPoint;
+                    ++internalCount;
+
+                    if (hasNext()) {
+                        currentPoint = points[internalCount];
+                    } else {
+                        currentPoint = null;
+                    }
+                    return pointToReturn;
+                }
+            };
+        }
+    }
+
+    @Override
+    public double apply(double x) {
+        synchronized (tabulatedFunction) {
+            return tabulatedFunction.apply(x);
+        }
+    }
+}

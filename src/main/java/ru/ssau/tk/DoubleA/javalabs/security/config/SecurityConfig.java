c58f17345d5adba1bc86ccd715a44ed0cf6cf708
Alex Davidchuk <sasha.d1990@list.ru>
2024-11-21 18:11:52 +0400

    Edit security config

--- src/main/java/ru/ssau/tk/DoubleA/javalabs/security/config/SecurityConfig.java
+++ src/main/java/ru/ssau/tk/DoubleA/javalabs/security/config/SecurityConfig.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,7 +13,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import ru.ssau.tk.DoubleA.javalabs.security.jwt.JWTFilter;
 
@@ -44,7 +41,6 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/auth/**").anonymous()
                         .requestMatchers("/users/whoami").authenticated()
                         .requestMatchers("/users/**").hasAuthority("ADMIN")
-                        .requestMatchers(HttpMethod.PUT, "/users/**").hasAuthority("ADMIN")
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
